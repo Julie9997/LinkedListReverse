e16e817c52c168ca244a6c4ed707fe3f12d6fb7e
Юлия <uliya.fomina99@gmail.com>
2023-06-29 15:45:21 +0300

    LinkedList reverse

---
+++ src/main/java/ru/julie/LinkedList.java
@@ -0,0 +1,18 @@
+package ru.julie;
+
+public class LinkedListReverse {
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
